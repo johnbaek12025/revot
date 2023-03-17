3af09ba895a86403f7ca871202ef25888933ef77
root <root@172-105-199-170.ip.linodeusercontent.com>
2023-03-17 13:59:15 +0900

    modified error

--- manager/urls.py
+++ manager/urls.py
@@ -9,8 +9,8 @@ app_name = 'devoperator'
 urlpatterns = [
     path('user/', LoggedIn([1, 2])(UserManage.as_view(http_method_names=['get'])), name='user-data'), 
     path('user/auth/', LoggedIn([1, 2])(UserManage.as_view(http_method_names=['put'])), name='authorization'),     
-    path('check/review/ticket/', LoggedIn([2], True)(TicketManage.as_view(http_method_names=['get', 'put'])), name='review-ticket'),
-    path('check/purchase/ticket/', LoggedIn([2], True)(TicketManage.as_view(http_method_names=['get', 'put'])), name='purchase-ticket'),
+    path('check/review/ticket/', LoggedIn([2], True)(TicketManage.as_view(http_method_names=['get', 'post'])), name='review-ticket'),
+    path('check/purchase/ticket/', LoggedIn([2], True)(TicketManage.as_view(http_method_names=['get', 'post'])), name='purchase-ticket'),
     path('check/review/ticket/count/', LoggedIn([2], True)(TicketManage.as_view(http_method_names=['get'])), name='review-ticket-count'),
     path('check/purchase/ticket/count/', LoggedIn([2], True)(TicketManage.as_view(http_method_names=['get'])), name='purchase-ticket-count'),
     path('ticket/reject/', LoggedIn([2], True)(TicketManage.as_view(http_method_names=['put'])), name='reject-ticket-request'),
